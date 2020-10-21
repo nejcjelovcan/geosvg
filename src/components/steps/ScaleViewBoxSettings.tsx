ef10a4e6eab432574bf0ced3a7d21a4dab79b1ac
Nejc Jelovčan <nejc.jelovcan@gmail.com>
2020-10-21 18:18:00 +0200

    Removed a couple of console.logs

--- src/components/steps/ScaleViewBoxSettings.tsx
+++ src/components/steps/ScaleViewBoxSettings.tsx
@@ -11,7 +11,6 @@ const ScaleViewBoxSettings = ({
   onChange,
   transform,
 }: ScaleViewBoxSettingsProps) => {
-  console.log("TRANSFORM", transform);
   return (
     <>
       <div className={stepStyles.settingsPair}>
