90de81446dfecfaf0e8fd28b8acb3b3bca41a41f
Nejc Jelovčan <nejc.jelovcan@gmail.com>
2020-10-22 17:04:10 +0200

    Finishing touches:
    - updated visuals for static cards
    - s3 caching
    - lint scripts

--- src/index.tsx
+++ src/index.tsx
@@ -10,15 +10,10 @@ import { Provider } from "overmind-react";
 const overmind = createOvermind(config);
 
 ReactDOM.render(
-  // <React.StrictMode> There are some blueprintjs issues with StrictMode
   <Provider value={overmind}>
     <App />
   </Provider>,
-  // </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
